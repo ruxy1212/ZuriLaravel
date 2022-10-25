78b60882882e50dca5fb4ef58bb0f83fc57fbf8e
ruxy1212 <russelloje@gmail.com>
2022-10-25 10:08:57 +0100

    Week 6 Laravel Task

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>ZuriLaravel</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Hello Laravel!</h1>
+            <p>This is the first application that I'm trying to build with laravel framework.</p>
+        </div>
+    </body>
+</html>
